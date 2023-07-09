481e3109525559b5dbacc3f4775eb2a425b5a98b
Dingdong <dliuak@connect.ust.hk>
2023-07-09 18:23:10 +0800

    add: a fake loop to discuss the mainloop

---
+++ fake_loop.py
@@ -0,0 +1,13 @@
+def mainloop():
+    # 1. initialize all loggers
+    # 2. instantiate sensors
+    # 3. construct the first turn object and start initial greetings with a magic string
+    # 4. receive language from patient
+    # 5. monitor state change and construct a turn object
+    # 6. engagement estimator
+    # 7. pass to robot
+    # 8. construct a action object
+    # 9. acquire robot turn
+    # 10. execution
+    # Goto step 4
+    pass
