211edd2079057361e1662dcba563d0f3b7b03506
TheDoctor-JI <yifanchangle@163.com>
2024-02-01 14:56:41 +0800

    fixed logging bug

--- utils/dialogflow_connector.py
+++ utils/dialogflow_connector.py
@@ -151,7 +151,7 @@ class DialogflowConnector:
             self.logger.info('REVERT COMMAND SKIPPED')
             return
         self.consecutive_revert_flag = True
-        self.logger.info("Revert previous sentence with magic string and Intent: '%s', '%s'", self.revert_magic_string)
+        self.logger.info("Revert previous sentence with magic string: '%s'", self.revert_magic_string)
         response = self.communicate(asr_text=self.revert_magic_string)
         return response
 
