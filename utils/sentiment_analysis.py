e2656e1de976ae5a37c21d80a4718216c631c4dc
Dingdong-LIU <dliuak@connect.ust.hk>
2024-07-23 11:21:54 +0800

    add: move to gpu

--- utils/sentiment_analysis.py
+++ utils/sentiment_analysis.py
@@ -1,15 +1,20 @@
 import time
-from flask import Flask, request, jsonify
+import os
 import torch
-from transformers import T5Tokenizer, AutoModelForSeq2SeqLM, GenerationConfig
+from flask import Flask, jsonify, request
+from transformers import AutoModelForSeq2SeqLM, GenerationConfig, T5Tokenizer
 
 app = Flask(__name__)
+
+
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 tokenizer = T5Tokenizer.from_pretrained(
     "yuyijiong/T5-large-sentiment-analysis-Chinese-MultiTask"
 )
 model = AutoModelForSeq2SeqLM.from_pretrained(
-    "yuyijiong/T5-large-sentiment-analysis-Chinese-MultiTask"
+    "yuyijiong/T5-large-sentiment-analysis-Chinese-MultiTask", device="cuda"
 )
 generation_config = GenerationConfig.from_pretrained(
     "yuyijiong/T5-large-sentiment-analysis-Chinese-MultiTask"
@@ -25,20 +30,22 @@ def predict():
     input_text = f"判断以下对问题回答的情感极性: {data['conversation']}"
     result = "neutral"
     # Perform inference using your T5 model
-    input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+    input_ids = tokenizer(input_text, return_tensors="pt").input_ids.cuda()
     with torch.no_grad():
         output = model.generate(input_ids, generation_config=generation_config)
     output_str = tokenizer.batch_decode(output, skip_special_tokens=True)
-    if '消极' in output_str:
+    if "消极" in output_str:
         result = "negative"
-    elif '中性' in output_str:
+    elif "中性" in output_str:
         result = "neutral"
-    elif '积极' in output_str:
+    elif "积极" in output_str:
         result = "positive"
     # log the input and output to console
     end_time = time.time()
-    print(f"Input: {input_text}\n Output: {output_str} \n Execution time: {end_time - start_time} seconds")
-    
+    print(
+        f"Input: {input_text}\n Output: {output_str} \n Execution time: {end_time - start_time} seconds"
+    )
+
     return jsonify({"output": result})
 
 
