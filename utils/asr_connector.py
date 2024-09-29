8ce464b0677ce9aed33f0015be5c03eccb75aad2
Dingdong-LIU <dliuak@connect.ust.hk>
2024-09-29 10:38:31 +0800

    add: draft for new sentence stream

--- utils/asr_connector.py
+++ utils/asr_connector.py
@@ -135,3 +135,87 @@ class ASR_Interim_Sentence:
         if timeout:
             self.logger.error("%s ASR timeout, return previous sentence '%s'", self.__class__.__name__, sentence)
         return (sentence, timeout)
+    
+
+class ASR_New_Sentence:
+    """
+    This class listens to ASR interim sentence.
+    """
+
+    def __init__(self, args) -> None:
+        # Subscriber listening to ROS topic
+        # rospy.init_node("ASR_Full_Sentence_Node")
+        self.full_sentence_listener = rospy.Subscriber(
+            args["ASRVAD"]["asr_interim_speech_topic"],
+            hr_msgs.msg.ChatMessage,
+            self.ASR_sentence_callback,
+            queue_size=100,
+        )
+        # Variables to be filled
+        self.asr_full_sentence = ""
+        self.sentence_format = {
+            "lang": "",
+            "confidence": 0,
+            "source": "",
+            "audio_path": "",
+        }
+        self.logger = logging.getLogger(__name__)
+
+        self.new_sentence = False
+        self.timestamp = 0
+
+    def ros_dynamic_configuration(self, lang="HK"):
+        """Dynamic configuration of ASR language settings,
+        choose between Cantonese yue-Hant-HK and English en-GB"""
+        # rospy.init_node('myconfig_py', anonymous=True)
+        client = dynamic_reconfigure.client.Client("/hr/perception/speech_recognizer")
+        if lang == "EN":
+            params = {"enable": True, "language": "en-GB"}  #'en-GB'
+        else:
+            params = {"enable": True, "language": "yue-Hant-HK"}  #'yue-Hant-HK'
+        config = client.update_configuration(params)
+        return config
+
+    def ASR_sentence_callback(self, msg):
+        # Update the asr message storage
+        self.asr_full_sentence = msg.utterance
+        self.sentence_format["lang"] = msg.lang
+        self.sentence_format["confidence"] = msg.confidence
+        self.sentence_format["source"] = msg.source
+        self.sentence_format["audio_path"] = msg.audio_path
+
+        self.logger.info("%s: '%s' ", self.__class__.__name__, self.asr_full_sentence)
+
+        self.new_sentence = True
+        self.timestamp = time.time()
+
+    def get_full_sentence(self):
+        if self.new_sentence:
+            self.new_sentence = False
+            wait = False
+            return (wait, self.asr_full_sentence)
+        else:
+            wait = True
+            return (wait, self.asr_full_sentence)
+
+    def get_time_stamp(self):
+        output = (self.new_sentence, self.timestamp)
+        self.new_sentence = False
+        return output
+
+    def get_current_sentence(self):
+        start_waiting_time = time.time()
+        timeout = False
+        while not timeout:
+            wait, sentence = self.get_full_sentence()
+            if not wait:
+                return (sentence, timeout)
+            # update timeout
+            timeout = (time.time() - start_waiting_time) > 1
+        if timeout:
+            self.logger.error(
+                "%s ASR timeout, return previous sentence '%s'",
+                self.__class__.__name__,
+                sentence,
+            )
+        return (sentence, timeout)
